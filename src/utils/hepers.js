7990fd7a6e6456c1008d833a877ac80f056fccc7
cloudle <lehaoson@gmail.com>
2018-09-27 03:39:07 +0700

    correct clone typo..

--- src/utils/hepers.js
+++ src/utils/hepers.js
@@ -1,7 +1,7 @@
 export function count(string, substr) {
-	if !substring.length return 1/0;
+	if (!substring.length) return 1/0;
 
-	const num = pos = 0;	
+	let num = 0, pos = 0;
 	while (pos = 1 + string.indexOf(substr, pos)) num++;
 	return num;
 }
