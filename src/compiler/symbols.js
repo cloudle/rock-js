52d68ecc1051d9d65a8c344fbfb96632b99305c8
cloudle <lehaoson@gmail.com>
2018-10-02 03:25:34 +0700

    First crash version of Parser program..

--- src/compiler/symbols.js
+++ src/compiler/symbols.js
@@ -1,9 +1,24 @@
-export const Operator = Symbol('op');
+export const Program = Symbol('program');
+export const Operator = Symbol('operator');
 export const Number = Symbol('num');
 export const String = Symbol('string');
+export const Boolean = Symbol('boolean');
+export const Call = Symbol('call');
+export const If = Symbol('if');
 export const Keyword = Symbol('keyword');
 export const Identifier = Symbol('identifier');
 export const Puntuation = Symbol('punctuation');
+
+export const FALSE = { type: Boolean, value: false };
+export const TRUE = { type: Boolean, value: true };
+export const PRECEDENCE = {
+	'=': 1,
+	'||': 2,
+	'&&': 3,
+	'<': 7, '>': 7, '<=': 7, '>=': 7, '==': 7, '!=': 7,
+	'+': 10, '-': 10,
+	'*': 20, '/': 20, '%': 20,
+};
 
 export const keywords = [
 	'const', 'let',
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { InputStream, TokenStream } from './compiler/lexer';
+import { InputStream, TokenStream, parse } from './compiler/lexer';
 
-const sourceCode = 'let userName = "Cloud"';
+const sourceCode = `let userName = "Cloud"`;
 
 module.exports = () => {
 	let next;
 	const tokenStream = TokenStream(InputStream(sourceCode));
-	while (next = tokenStream.next()) console.log(next);
+	// while (next = tokenStream.next()) console.log(next);
+	console.log(parse(tokenStream));
 // 	const lexer = new Lexer();
 // 	console.log(lexer.tokenize(
 // `console.log "hello-world"`));
