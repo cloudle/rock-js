5a0671d14ab7c7292f7309251f19cf34ce3373d2
cloudle <lehaoson@gmail.com>
2018-10-07 04:11:12 +0700

    Update example, reformat code..

--- src/compiler/environment.js
+++ src/compiler/environment.js
@@ -1,30 +1,33 @@
-export class Environtment {
+export class Environment {
 	constructor(parent) {
 		this.vars = Object.create(parent ? parent.vars : null);
 		this.parent = parent;
 	}
 
 	extend() {
-		return new Environtment(this);
+		return new Environment(this);
 	}
 
 	lookup(name) {
 		let scope = this;
 
 		while (scope) {
-			if (Object.prototype.hasOwnProperty.call(scope.vars, name)) return scope;
+			if (Object.prototype.hasOwnProperty.call(scope.vars, name))
+				return scope;
 			scope = scope.parent;
 		}
 	}
 
 	get(name) {
-		if (name in this.vars) return this.vars[name];
+		if (name in this.vars)
+			return this.vars[name];
 		throw new Error(`Undefined variable ${name}`);
 	}
 
 	set(name, value) {
 		let scope = this.lookup(name);
-		if (!scope && this.parent) throw new Error(`Undefined variable ${name}`);
+		if (!scope && this.parent)
+			throw new Error(`Undefined variable ${name}`);
 		return (scope || this).vars[name] = value;
 	}
 
@@ -33,5 +36,5 @@ export class Environtment {
 	}
 }
 
-export default Environtment;
+export default Environment;
 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,47 @@
 import { InputStream } from './compiler/lexer';
 import { TokenStream } from './compiler/tokenizer';
 import parse from './compiler/parser';
-import { Environtment } from './compiler/environment';
+import { Environment } from './compiler/environment';
 import { evaluate } from './compiler/evaluate';
 
 const sourceCode = `
-print_range = λ(a, b) if a <= b {
-                        print(a);
-                        if a + 1 <= b {
-                          print(", ");
-                          print_range(a + 1, b);
-                        } else println("");
-                      };
-print_range(1, 20);
+print_range = λ(a, b) 
+	if a <= b {
+		print(a);
+		if a + 1 <= b {
+			print(", ");
+			print_range(a + 1, b);
+		} else println("");
+	};
+
+#print_range(1, 9);
+
+cons = λ(a, b) λ(f) f(a, b);
+car = λ(cell) cell(λ(a, b) a);
+cdr = λ(cell) cell(λ(a, b) b);
+NIL = λ(f) f(NIL, NIL);
+
+foreach = λ(list, f)
+	if list != NIL {		
+		f(car(list));
+		foreach(cdr(list), f);
+	};
+
+x = cons(1, cons(2, cons(3, cons(4, cons(5, NIL)))));
+foreach(x, println);
 `;
 
 module.exports = () => {
-	const code = "sum = lambda(x, y) x + y; print(sum(3, 3));",
-		ast = parse(TokenStream(InputStream(sourceCode))),
-		globalEnv = new Environtment();
+	const ast = parse(TokenStream(InputStream(sourceCode))),
+		globalEnv = new Environment();
 
 	globalEnv.def('print', (content) => process.stdout.write(`${content}`));
-	globalEnv.def('println', (content) => console.log(`\n${content}`));
+	globalEnv.def('println', (content) => console.log(content));
 	evaluate(ast, globalEnv);
-
 	// console.log(tokenStream.peek());
 	// while (next = tokenStream.next()) console.log(next);
 	// console.log(parse(tokenStream).program);
-// 	const lexer = new Lexer();
-// 	console.log(lexer.tokenize(
-// `console.log "hello-world"`));
+	// 	const lexer = new Lexer();
+	// 	console.log(lexer.tokenize(
+	// `console.log "hello-world"`));
 };
